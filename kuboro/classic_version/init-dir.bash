8c6d21deceb78a1040e01c16469b02fefca91651
Mathis ROME <mathis.rome@icloud.com>
2025-07-22 19:42:37 +0200

    feat(volume): local instead of hostpath

---
+++ kuboro/classic_version/init-dir.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /orocommerce/php-fpm/app
+mkdir -p /orocommerce/php-fpm/cache
+mkdir -p /orocommerce/php-fpm/maintenance
+mkdir -p /orocommerce/php-fpm/multihost-operation
+mkdir -p /orocommerce/php-fpm/private
+mkdir -p /orocommerce/php-fpm/public
