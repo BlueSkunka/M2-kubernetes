46cdd865bde823a6301022120f790492fb519049
BlueSkunka <quentin.somveille3874@gmail.com>
2025-07-24 20:33:04 +0200

    feat(csi): use of NFS

--- kuboro/classic_version/setup-project.bash
+++ kuboro/classic_version/setup-project.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 kubectl apply -f ./init
+curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/v4.11.0/deploy/install-driver.sh | bash -s v4.11.0 --
 kubectl apply -f ./tls
 kubectl apply -f ./volumes
 kubectl apply -f ./application/database
