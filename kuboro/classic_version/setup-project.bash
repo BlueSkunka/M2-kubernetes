43ce13912e5aa90c9a24d49f03604f012e3a7534
Mathis ROME <mrome@webnet.fr>
2025-07-24 17:38:29 +0200

    wip(tls): redirect on some page not working

--- kuboro/classic_version/setup-project.bash
+++ kuboro/classic_version/setup-project.bash
@@ -4,7 +4,7 @@ kubectl apply -f ./init
 kubectl apply -f ./tls
 kubectl apply -f ./volumes
 kubectl apply -f ./application/database
-kubectl apply -f ./application/restore-job.yaml
-kubectl wait --for=condition=complete job/orocommerce-restore-job -n orocommerce --timeout=1h
+kubectl apply -f ./application/app-restore-job.yaml
+kubectl wait --for=condition=complete job/orocommerce-app-restore-job -n orocommerce --timeout=1h
 kubectl apply -f ./application
-kubectl delete -f ./application/restore-job.yaml
+kubectl delete -f ./application/app-restore-job.yaml
