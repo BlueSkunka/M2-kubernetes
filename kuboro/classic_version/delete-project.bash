e98969bfd2f66a611d3e4df50113df8b30f8e9c9
BlueSkunka <quentin.somveille3874@gmail.com>
2025-07-26 17:20:24 +0200

    wip: nfs server

--- kuboro/classic_version/delete-project.bash
+++ kuboro/classic_version/delete-project.bash
@@ -5,4 +5,5 @@ kubectl delete -f ./application
 kubectl delete -f ./volumes
 kubectl apply -f ./tls
 curl -skSL https://raw.githubusercontent.com/kubernetes-csi/csi-driver-nfs/v4.11.0/deploy/uninstall-driver.sh | bash -s v4.11.0 --
+kubectl delete -f ./nfs_server
 kubectl delete -f ./init
