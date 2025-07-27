facdf09af5d245d91eef44f73fef537231562c37
Mathis ROME <mathis.rome@icloud.com>
2025-07-27 11:37:24 +0200

    fix(site): image dont load

--- kuboro/classic_version/apply-project.bash
+++ kuboro/classic_version/apply-project.bash
@@ -3,8 +3,11 @@
 kubectl apply -f ./init
 kubectl apply -f ./tls
 kubectl apply -f ./volumes
-kubectl apply -f ./application/database
-kubectl apply -f ./application/app-restore-job.yaml
-kubectl wait --for=condition=complete job/orocommerce-app-restore-job -n orocommerce --timeout=1h
 kubectl apply -f ./application
-kubectl delete -f ./application/app-restore-job.yaml
+
+# kubectl apply -f ./init
+# kubectl apply -f ./tls
+# kubectl apply -f ./volumes
+# kubectl apply -f ./application/postgre-statefulset.yaml
+# kubectl apply -f ./application/postgre-service.yaml
+# kubectl apply -f ./application/app-restore-job.yaml
--- kuboro/classic_version/delete-project.bash
+++ kuboro/classic_version/delete-project.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-kubectl delete -f ./application/database
 kubectl delete -f ./application
 kubectl delete -f ./volumes
 kubectl delete -f ./tls
