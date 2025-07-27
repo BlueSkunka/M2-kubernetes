ab3be59905f90bcde5cf5669edf23b05c7bfeac2
Mathis ROME <mathis.rome@icloud.com>
2025-07-27 14:40:48 +0200

    feat(helm): setup all helm

--- kuboro/classic_version/apply-project.bash
+++ kuboro/classic_version/apply-project.bash
@@ -4,10 +4,3 @@ kubectl apply -f ./init
 kubectl apply -f ./tls
 kubectl apply -f ./volumes
 kubectl apply -f ./application
-
-# kubectl apply -f ./init
-# kubectl apply -f ./tls
-# kubectl apply -f ./volumes
-# kubectl apply -f ./application/postgre-statefulset.yaml
-# kubectl apply -f ./application/postgre-service.yaml
-# kubectl apply -f ./application/app-restore-job.yaml
