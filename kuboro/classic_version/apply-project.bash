18e0b0b656e45e5813ce8f40d9ca078dbdd177b1
Mathis ROME <mathis.rome@icloud.com>
2025-07-27 18:54:02 +0200

    feat(grafana-prometheus): repo add

--- kuboro/classic_version/apply-project.bash
+++ kuboro/classic_version/apply-project.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm install prometheus prometheus-community/kube-prometheus-stack
 kubectl apply -f ./tls/grafana-tls-secret.yaml
 kubectl apply -f ./application/grafana-ingress.yaml
