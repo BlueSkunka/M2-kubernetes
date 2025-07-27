acdbe40e536fe5f68a48ea5b8c18aa59c4e00a60
Mathis ROME <mathis.rome@icloud.com>
2025-07-27 18:51:27 +0200

    feat(grafana-prometheus)

--- kuboro/classic_version/apply-project.bash
+++ kuboro/classic_version/apply-project.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+helm install prometheus prometheus-community/kube-prometheus-stack
+kubectl apply -f ./tls/grafana-tls-secret.yaml
+kubectl apply -f ./application/grafana-ingress.yaml
 kubectl apply -f ./init
 kubectl apply -f ./tls
 kubectl apply -f ./volumes
--- kuboro/classic_version/delete-project.bash
+++ kuboro/classic_version/delete-project.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+helm uninstall prometheus
 kubectl delete -f ./application
 kubectl delete -f ./volumes
 kubectl delete -f ./tls
