2e2d9ad390c34755244870644a7b61f60536784a
Mathis ROME <mathis.rome@icloud.com>
2025-07-31 21:13:47 +0200

    feat(prom-grafana): ingress traefik instead of nginx

--- prom_grafana/helm_version/apply-prom-stack-helm.bash
+++ prom_grafana/helm_version/apply-prom-stack-helm.bash
@@ -3,4 +3,4 @@
 kubectl create namespace monitoring
 kubectl apply -f grafana-tls-secret.yaml
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm install -n monitoring -f values.yml prometheus prometheus-community/kube-prometheus-stack
+helm install -n monitoring -f values.yaml prometheus prometheus-community/kube-prometheus-stack
